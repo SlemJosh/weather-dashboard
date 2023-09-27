f7577e236aaaba60008bd1d25f321c4633fbef29
SlemJosh <joshua.slem@gmail.com>
2023-09-27 13:41:05 -0600

    finished some updates on the index and css, starting into our javascript file.

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Javascript
+// First thing is to put our Key from openweathermap.
+
+const API_KEY = "9d5dfbe83494f7a8091376c453d71c32";
+
+// Our first thing that needs attention is our search box, with both the button and the text entry field.
+
