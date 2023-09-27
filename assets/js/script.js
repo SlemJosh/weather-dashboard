93239a050329f9a2d62d33c85d1bb4a643bca6a1
SlemJosh <joshua.slem@gmail.com>
2023-09-27 13:45:52 -0600

    Created a function that will gather data from our search field, and that will pass our information onto another function that will search the weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,9 @@
 const API_KEY = "9d5dfbe83494f7a8091376c453d71c32";
 
 // Our first thing that needs attention is our search box, with both the button and the text entry field.
-
+// Here's our first function. We called it citySearch, and we declared a variable and set it to the value that will be input on the index page. We also called another function to take the data we pass it.
+function searchCity(){
+    var searchInput = $("#searchInput").val();
+    fetchWeatherAPI(searchInput);
+    $('#searchInput').val("");
+}
